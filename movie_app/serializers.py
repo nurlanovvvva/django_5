946839d61e5e7a65d591df8d1d8dc41d5be4859a
nurlanvvvva <ajtunuknurlanova@gmail.com>
2024-09-27 04:30:02 +0600

    homework_4

--- movie_app/serializers.py
+++ movie_app/serializers.py
@@ -7,11 +7,26 @@ class DirectorSerializer(serializers.ModelSerializer):
         model = Director
         fields = '__all__'
 
+    def validate_name(self, value):
+        if not value.strip():
+            raise serializers.ValidationError("Имя режиссера не может быть пустым.")
+        return value
+
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
+
+    def validate_title(self, value):
+        if not value.strip():
+            raise serializers.ValidationError("Название фильма не может быть пустым.")
+        return value
+
+    def validate_duration(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Продолжительность фильма должна быть положительным числом.")
+        return value
 
 
 class ReviewSerializer(serializers.ModelSerializer):
@@ -19,6 +34,15 @@ class ReviewSerializer(serializers.ModelSerializer):
         model = Review
         fields = '__all__'
 
+    def validate_text(self, value):
+        if not value.strip():
+            raise serializers.ValidationError("Отзыв не может быть пустым.")
+        return value
+
+    def validate_stars(self, value):
+        if value < 1 or value > 5:
+            raise serializers.ValidationError("Рейтинг должен быть от 1 до 5.")
+        return value
 
 class MovieWithReviewsSerializer(serializers.ModelSerializer):
     reviews = ReviewSerializer(many=True)
